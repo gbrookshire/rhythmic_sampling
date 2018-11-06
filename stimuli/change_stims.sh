eda86a17f05d2e7c9e6af9607389685f3230731b
Geoff Brookshire <g.brookshire@bham.ac.uk>
2018-11-06 15:06:01 +0000

    commented image manipulation script

--- stimuli/change_stims.sh
+++ stimuli/change_stims.sh
@@ -1,2 +1,2 @@
-NPIC=1 # Change this number 1--6
+NPIC=1 # Change this number 1--6 (too lazy to implement loop in bash)
 convert orig/greyFace${NPIC}_V3-01.jpg -sigmoidal-contrast 20,60% -resize 256x256 ${NPIC}.jpg
