e9144b219e53c68dda6291ffacb7528ceb0831b2
Geoff Brookshire <g.brookshire@bham.ac.uk>
2018-11-28 15:09:43 +0000

    Get grad struct from real data, not Quest

--- analysis/rs_grad_struct.m
+++ analysis/rs_grad_struct.m
@@ -1,9 +1,11 @@
 function rs_grad_struct(i_subject)
 
+% Save grad structures for each subject (only from one block of trials)
+
 rs_setup
 
 fname = subject_info.meg{i_subject};
-dataset = [exp_dir 'raw/' fname '/1.fif'];
+dataset = [exp_dir 'raw/' fname '/3.fif'];
 hdr = ft_read_header(dataset);
 grad = hdr.grad;
 [~,~,~] = mkdir([exp_dir 'grad/'], fname);
