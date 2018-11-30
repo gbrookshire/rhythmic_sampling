daa9d482133873b75052749bbd0d489a885b431a
Geoff Brookshire <g.brookshire@bham.ac.uk>
2018-11-30 10:26:17 +0000

    New function for getting hits/misses

---
+++ analysis/rs_resptype.m
@@ -0,0 +1,16 @@
+function [hits nans] = rs_resptype(i_subject)
+
+% Return a vector of hits/misses/etc
+
+rs_setup
+fname = subject_info.meg{i_subject};
+
+hits = [];
+nans = [];
+for i_rec = block_info.main
+    % Read in the trial definition
+    fn = [exp_dir 'trialdef/' fname '/' num2str(i_rec) '.mat'];
+    trialdef = load(fn);
+    hits = [hits trialdef.trl.trial(:,4)'];
+    nans = [nans isnan(trialdef.trl.target(:,1))'];
+end
