bba8c71a3edde6519f3a3c603eaeb6bb60d62d40
Geoffrey Brookshire <geoff.brookshire@gmail.com>
2018-12-19 15:37:59 +0000

    Fix algorithm and change from GEDb to RESS

--- analysis/rs_gedb.m
+++ analysis/rs_gedb.m
@@ -3,6 +3,7 @@ function [y, A] = rs_gedb(data, f_low, f_high)
 % Generalized eigendecomposition, broadband
 % Adapted from
 %   https://github.com/svendaehne/matlab_SPoC/blob/master/SSD/ssd.m
+% --- This script mixes together RESS and GEDb. Switch to just RESS
 
 % Append all trials into one big matrix
 x = cat(2, data.trial{:});
@@ -16,16 +17,36 @@ signal_band = [f_low, f_high];
 x_filt = filtfilt(b, a, x); % Does this need to be transposed?
 cov_filt = cov(x_filt', 1);
 
-% Get the joint eigenvalues of cov_bb' * cov_filt
-[V, D] = eig(cov_bb' * cov_filt); % GEDb
-%%%% Should be the same as the following...? (from Gulbanaite & Cohen
+% Run the generalized eigendecomposition
+[V, D] = eig(inv(cov_bb) * cov_filt); % GEDb
 [V, D] = eig(cov_filt, cov_bb); % RESS
+
+% % Two equivalent ways to compute generalized eigendecomposition:
+% % Derivation:
+% % [V,D] = eig(A, B)
+% % A*V = B*V*D
+% % inv(B)*A*V = V*D
+% % Which is equivalent to normal eigendecomposition if inv(B)*A is treated
+% % as a single matrix C: C*V = V*D
+% % [V,D] = eig(inv(B), A)
+% % Example:
+% A = rand(10,2);
+% B = rand(10,2);
+% A = cov(A);
+% B = cov(B);
+% [V1,D1] = eig(A, B); % First way
+% [V2,D2] = eig(inv(B) * A); % Second way
+% rounding_error = 1e-10;
+% all(D1 - D2 < rounding_error) % Eigenvalues are almost identical
+% V1 ./ V2 % Eigenvectors are the same after multiplying by a scalar
+% % The first method (eig(A, B)) is better for 'numerical stability'
+% according to Gulbinaite and Cohen (2017).
 
 % Sort the eigenvalues/vectors
 [~, sort_idx] = sort(diag(D), 'descend');
 W = V(:, sort_idx);
 
-% Get the timecourse of the GEDb spatial filter
+% Get the timecourse of the spatial filter
 y = W(:,1) * x;
 
 % Get the matrix of the patterns (in columns)
