2e181064e5dcfa64c532e882a10e676961ed6b16
Geoffrey Brookshire <geoff.brookshire@gmail.com>
2018-12-21 11:03:00 -0800

    Shifted to RESS by adapting code from CGalbinaite & Cohen

--- analysis/rs_ress.m
+++ analysis/rs_ress.m
@@ -1,55 +1,62 @@
-function [y, A] = rs_gedb(data, f_low, f_high)
+function [data_out, maps] = rs_ress(data_in, f, fwhm)
 
-% Generalized eigendecomposition, broadband
-% Adapted from
-%   https://github.com/svendaehne/matlab_SPoC/blob/master/SSD/ssd.m
-% --- This script mixes together RESS and GEDb. Switch to just RESS
+% RESS (Gulbinaite & Cohen, 2017 NeuroImage)
+% Spatial filter for extracting periodic activity
+%
+% INPUTS
+%   data_in: A fieldtrip data structure (from preprocessing)
+%   f: peak frequency of filter
+%   fhwm: standard deviation of filter, 
+%         defined as full-width at half-maximum in Hz
+%
+% OUTPUTS
+%   data_out: A fieldtrip data structure of activity at the RESS filter
+%   maps: Scalp topography of the filters
 
-% Append all trials into one big matrix
-x = cat(2, data.trial{:});
+
+% Append all trials into one big matrix (Channel * Time)
+x = cat(2, data_in.trial{:});
+
+% Mean-center each row before getting covariance matrix
+center = @(x) bsxfun(@minus, x, mean(x, 2));
 
 % Get covariance matrix of the broadband activity
-cov_bb = cov(x', 1);
+cov_bb = cov(center(x)', 1);
 
 % Get covariance matrix of narrowband-filtered activity
-signal_band = [f_low, f_high];
-[b,a] = butter(filter_order, signal_band / (data.fsample / 2));
-x_filt = filtfilt(b, a, x); % Does this need to be transposed?
-cov_filt = cov(x_filt', 1);
+% signal_band = [f_low, f_high];
+% [b,a] = butter(filter_order, signal_band / (data.fsample / 2));
+% x_filt = filtfilt(b, a, x); % Does this need to be transposed?
+x_filt = filterFGx(x, data_in.fsample, f, fwhm);
+cov_filt = cov(center(x_filt)', 1);
 
-% Run the generalized eigendecomposition
-[V, D] = eig(inv(cov_bb) * cov_filt); % GEDb
-[V, D] = eig(cov_filt, cov_bb); % RESS
+% perform generalized eigendecomposition
+[evecs, evals] = eig(cov_filt, cov_bb);
+% find maximum component
+[~, comp2plot] = max(diag(evals));
+% normalize vectors (not really necessary, but OK)
+evecs = bsxfun(@rdivide, evecs, sqrt(sum(evecs .^ 2, 1)));
 
-% % Two equivalent ways to compute generalized eigendecomposition:
-% % Derivation:
-% % [V,D] = eig(A, B)
-% % A*V = B*V*D
-% % inv(B)*A*V = V*D
-% % Which is equivalent to normal eigendecomposition if inv(B)*A is treated
-% % as a single matrix C: C*V = V*D
-% % [V,D] = eig(inv(B), A)
-% % Example:
-% A = rand(10,2);
-% B = rand(10,2);
-% A = cov(A);
-% B = cov(B);
-% [V1,D1] = eig(A, B); % First way
-% [V2,D2] = eig(inv(B) * A); % Second way
-% rounding_error = 1e-10;
-% all(D1 - D2 < rounding_error) % Eigenvalues are almost identical
-% V1 ./ V2 % Eigenvectors are the same after multiplying by a scalar
-% % The first method (eig(A, B)) is better for 'numerical stability'
-% according to Gulbinaite and Cohen (2017).
+% extract components and force sign
+% maps = inv(evecs'); % get maps (this is fine for full-rank matrices)
+% Get maps in a way that's robust to rank-deficiency
+maps = cov_filt * evecs / (evecs' * cov_filt * evecs);
+[~,idx] = max(abs(maps(:, comp2plot))); % find biggest component
+maps = maps * sign(maps(idx, comp2plot)); % force to positive sign
 
-% Sort the eigenvalues/vectors
-[~, sort_idx] = sort(diag(D), 'descend');
-W = V(:, sort_idx);
+% reconstruct RESS component time series
+ress_ts1 = zeros(EEG.pnts, size(data,3));
+for ti=1:size(data,3)
+    ress_ts1(:,ti) = evecs(:,comp2plot)' * squeeze(data(:,:,ti));
+end
 
-% Get the timecourse of the spatial filter
-y = W(:,1) * x;
-
-% Get the matrix of the patterns (in columns)
-A = cov_filt * W / (W' * cov_filt * W);
+% Make Fieldtrip-style object to return out
+data_out = data_in;
+data_out.label = {'RESS'};
+data_out.trial = cell(size(data_in.trial));
+for i_trial = 1:length(data_in.trial)
+    trial_data = data_in.trial{i_trial};
+    data_out.trial{i_trial} = evecs(:, comp2plot)' * squeeze(trial_data);
+end
 
 end
