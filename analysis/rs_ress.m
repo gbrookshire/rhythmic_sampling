e2e88e249e95699d3d65cbc29a58f3a5ab22ba30
gbrookshire <geoff.brookshire@gmail.com>
2019-01-10 18:53:05 +0000

    Tests - topography of RESS maps

--- analysis/rs_ress.m
+++ analysis/rs_ress.m
@@ -13,23 +13,29 @@ function [data_out, maps] = rs_ress(data_in, f, fwhm)
 %   data_out: A fieldtrip data structure of activity at the RESS filter
 %   maps: Scalp topography of the filters
 
+% % Setup script for testing
+% cd('C:\Users\brookshg\Documents\rhythmic_sampling\rhythmic_sampling\analysis')
+% addpath('C:\Users\brookshg\Documents\fieldtrip-20180805');
+% load('C:\Users\brookshg\Documents\rhythmic_sampling\sample_data\preproc\trial\181009_b46d\181009\preproc.mat')
 
 % Append all trials into one big matrix (Channel * Time)
 x = cat(2, data_in.trial{:});
 
-% Normalize matrix
-x = normalize(x)
+if rank(x') ~= size(x,1)
+    warning('The input data are rank-deficient')
+end
+% The full data are rank-deficient, but I'm not sure why. When I select
+% only the magnetometers, or only the gradiometers, the data have full
+% rank. Why does this happen?
 
 % Mean-center each row before getting covariance matrix
+% This only affects the covariance matrices very weakly (1e-38)
 center = @(x) bsxfun(@minus, x, mean(x, 2));
 
 % Get covariance matrix of the broadband activity
-cov_bb = cov(center(normalize(x))', 1);
+cov_bb = cov(center(x)', 1);
 
 % Get covariance matrix of narrowband-filtered activity
-% signal_band = [f_low, f_high];
-% [b,a] = butter(filter_order, signal_band / (data.fsample / 2));
-% x_filt = filtfilt(b, a, x); % Does this need to be transposed?
 fsample = 1 / mean(diff(data_in.time{1}));
 x_filt = filterFGx(x, fsample, f, fwhm);
 cov_filt = cov(center(x_filt)', 1);
@@ -48,11 +54,11 @@ maps = cov_filt * evecs / (evecs' * cov_filt * evecs);
 [~,idx] = max(abs(maps(:, comp2plot))); % find biggest component
 maps = maps * sign(maps(idx, comp2plot)); % force to positive sign
 
-% reconstruct RESS component time series
-ress_ts1 = zeros(EEG.pnts, size(data,3));
-for ti=1:size(data,3)
-    ress_ts1(:,ti) = evecs(:,comp2plot)' * squeeze(data(:,:,ti));
-end
+% % reconstruct RESS component time series
+% ress_ts1 = zeros(EEG.pnts, size(data,3));
+% for ti=1:size(data,3)
+%     ress_ts1(:,ti) = evecs(:,comp2plot)' * squeeze(data(:,:,ti));
+% end
 
 % Make Fieldtrip-style object to return out
 data_out = data_in;
@@ -64,3 +70,28 @@ for i_trial = 1:length(data_in.trial)
 end
 
 end
+
+%% Testing
+
+% Plot the maps -- right now they make no sense
+
+n_maps = size(maps,1);
+d = [];
+d.fsample = data_out.fsample;
+d.label = data_in.label;
+d.time = 1:n_maps; % Actually not time, but component number
+d.avg = maps;
+d.dimord = 'chan_time';
+
+for i_map = 1:9
+    subplot(3, 3, i_map)
+    cfg = [];
+    cfg.marker = 'off';
+    cfg.comment = 'no';
+    cfg.style = 'straight';
+    cfg.layout = chan.grad.layout;
+    cfg.xlim = [-0.1 0.1] + i_map;
+    ft_topoplotER(cfg, d)
+end
+
+% Check the spectra of the time-course
