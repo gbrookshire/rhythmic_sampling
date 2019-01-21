4ed68391bc704b79c4ddc1cfe0f0f1d54672f710
Geoff Brookshire <g.brookshire@bham.ac.uk>
2019-01-21 14:09:35 +0000

    separate spectra for each RESS filter

--- analysis/rs_tagged_spect.m
+++ analysis/rs_tagged_spect.m
@@ -18,21 +18,22 @@ function rs_tagged_spect(i_subject, segment_type)
 rs_setup
 fname = subject_info.meg{i_subject};
 
-% Load the TFR
+% Load the data
+behav = rs_behavior(i_subject);
 d = load([exp_dir 'tfr/' segment_type '/' fname '/high']);
 d = d.high_freq_data;
 
-% Analyze temporal structure separately for each RESS filter
+% To facilitate FFT calculation, make a FT struct with a separate 'channel'
+% for each frequency at each RESS filter
 data_ress = [];
 for side = {'left' 'right'}
-    % Select RESS filter for one side
+    % Make a FT data struc of RESS filter for one side
     chan_inx = strcmp(side{1}, d.label);
-    % New data structure
     d_side = [];
-    d_side.fsample = 1 / mean(diff(d.time));
     d_side.trialinfo = d.trialinfo;
     % Set channel names as the frequencies
-    d_side.label = cellfun(@(x) num2str(x), num2cell(d.freq)', ...
+    d_side.label = cellfun(@(x) sprintf('%.6f', x), ...
+        num2cell(d.freq)', ...
         'UniformOutput', false);
     d_side.trial = cell([1 size(d.powspctrm, 1)]);
     d_side.time = cell([1 size(d.powspctrm, 1)]);
@@ -43,48 +44,47 @@ for side = {'left' 'right'}
         d_side.trial{i_rpt} = curr_rpt(:, active_samples);
     end
     
+    
     % Toss trials with no samples
-    % (How did this happen?)
+    % This happens when calculating TFR on short trials
     cfg = [];
     cfg.trials = cellfun('length', d_side.time) > 1;
     d_side = ft_selectdata(cfg, d_side);
-    
+
     % Split into short segments
     cfg = [];
     cfg.minlength = 1; % Toss segments smaller than 1 s
     cfg.length = 1; % Split into n-second segments
     cfg.overlap = 0.8; % Segments overlap by this prop
     d_side = ft_redefinetrial(cfg, d_side);
+
+    % Toss segments that overlap with or occur after the response
+    % Or that include the transient response at the beginning of the trial
+    includes_resp = nan(size(d_side.time));
+    beginning_of_trial = nan(size(d_side.time));
+    for i_rpt = 1:length(d_side.time)
+        n_trial = d_side.trialinfo(i_rpt, 2);
+        resp_time = behav.rt(behav.TrialNumber == n_trial);
+        t = d_side.time{i_rpt};
+        includes_resp(i_rpt) = resp_time < max(t);
+        beginning_of_trial(i_rpt) = min(t) < 0.5;
+    end
+    cfg = [];
+    cfg.trials = ~includes_resp & ~beginning_of_trial;
+    d_side = ft_selectdata(cfg, d_side);
+
+    % Compute the spectra
+    cfg = [];
+    cfg.method = 'mtmfft';
+    cfg.output = 'pow';
+    cfg.taper = 'hanning';
+    cfg.polyremoval = 1; % Remove linear trends
+    cfg.keeptrials = 'yes';
+    spectra = ft_freqanalysis(cfg, d_side);
     
-    
-    
-    %%%%%%%%%%%%% STOPPED HERE %%%%%%%%%%%%
-    
-    
-    data_ress.(side{1}) = d_side;
-    clear d_new;
+    data_ress.(side{1}) = spectra;
+    clear d_side spectra;
 end    
-
-
-% Find trials that overlap with the response, or occur after the response
-behav = rs_behavior(i_subject);
-includes_resp = nan(size(d.time));
-for i_rpt = 1:length(d.time)
-    n_trial = d.trialinfo(i_rpt, 2);
-    resp_time = behav.rt(behav.TrialNumber == n_trial);
-    t = d.time{i_rpt};
-    includes_resp(i_rpt) = (min(t) < resp_time);
-end
-
-% Compute the spectra
-cfg = [];
-cfg.trials = ~includes_resp; % Exclude segments that overlap with a resp
-cfg.method = 'mtmfft';
-cfg.output = 'pow';
-cfg.taper = 'hanning';
-cfg.polyremoval = 1; % Remove linear trends
-cfg.keeptrials = 'yes';
-spectra = ft_freqanalysis(cfg, d);
 
 save([exp_dir 'tfr/' segment_type '/' '/' fname '/spect'], 'spectra')
 
