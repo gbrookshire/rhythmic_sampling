aba8e94c8967cdba082cefa2bd2e8e1050c9d33b
gbrookshire <g.brookshire@bham.ac.uk>
2019-02-15 15:46:52 +0000

    Use all channels (not RESS) for LF TFR

--- analysis/rs_tfr.m
+++ analysis/rs_tfr.m
@@ -8,16 +8,14 @@ function rs_tfr(i_subject, segment_type)
 
 rs_setup
 
+fname = subject_info.meg{i_subject};
+
 step_size = 0.02; % Should be divisible by 1/Fs to preserve time-bins
 if strcmp(segment_type, 'trial')
     toi = -0.5:step_size:(exp_params.max_trial_dur + 0.5);
 else
     toi = -0.5:step_size:0.5;
 end
-
-% Load preprocessed data
-fname = subject_info.meg{i_subject};
-d = rs_preproc_ress(i_subject, segment_type);
 
 %{
 % SIMULATED DATA
@@ -39,6 +37,8 @@ cfg_base.keeptrials = 'yes';
 % cfg_base.padtype = 'zero';
 
 % TFR around the tagged frequencies
+For HF data, use virtual channels from RESS spatial filters
+d = rs_preproc_ress(i_subject, segment_type);
 time_window = 0.2; % Smaller window -> more freq smoothing
 cfg = cfg_base;
 cfg.output = 'pow';
@@ -46,9 +46,12 @@ cfg.foi = 55:100;
 cfg.t_ftimwin = ones(length(cfg.foi), 1).* time_window;
 high_freq_data = ft_freqanalysis(cfg, d);
 save([save_dir '/' fname '/high'], 'high_freq_data', '-v7.3')
-clear cfg high_freq_data
+clear d cfg high_freq_data
 
 % TFR at low freqs (theta, alpha)
+% For LF data, use all channels
+d = load([exp_dir 'preproc/' segment_type '/' fname '/preproc']);
+d = d.data;
 n_cycles = 2; % As in Fiebelkorn et al (2018, Neuron)
 cfg = cfg_base;
 cfg.output = 'fourier'; % Get phase with `angle(...)`
@@ -58,4 +61,3 @@ cfg.pad = 7; % Pad trials out to 7 sec
 cfg.padtype = 'mirror'; % Is this OK for estimating phase?
 low_freq_data = ft_freqanalysis(cfg, d);
 save([save_dir '/' fname '/low'], 'low_freq_data')
-   
