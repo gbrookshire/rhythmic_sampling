f1923d4a19f89db6b12188293f0511d12f86a257
gbrookshire <g.brookshire@bham.ac.uk>
2019-02-22 15:02:29 +0000

    Updated for Ubuntu desktop

--- analysis/rs_base_path.m
+++ analysis/rs_base_path.m
@@ -1,6 +1,9 @@
 % Set up paths for windows vs unix
+host_name = char(java.net.InetAddress.getLocalHost.getHostName);
 if strcmp(computer, 'PCWIN64')
     base_dir = 'Z:/gb/';
+elseif strcmp(host_name, 'colles-d164179')
+    base_dir = '/home/local/ADF/brookshg/rds_share/gb/';
 else
     base_dir = '/rds/projects/2017/jenseno-02/gb/';
 end
