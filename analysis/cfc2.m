c536f3c607fba6376d2e252266333bdf8c13fef7
gbrookshire <g.brookshire@bham.ac.uk>
2019-04-11 09:56:54 +0000

    Refactor: Compute for each chan separately to avoid memory issues. (Slower, but much less memory used)

--- analysis/cfc2.m
+++ analysis/cfc2.m
@@ -17,61 +17,62 @@ function [cfc_data, mod_freq] = cfc2(data, freq, nfft, width)
 fsample = data.fsample;
 width = width * ones(size(freq));
 
-% Compute the power time-course
-pwr = cell([1 length(data.trial)]);
-for i_trial = 1:length(data.trial)
-    p = nan(1, length(data.time{i_trial})); % Only 1 chan/freq at a time
-    for i_freq = 1:length(freq)
-        f = freq(i_freq);
-        N = floor(width(i_freq) * fsample / f);
-        taper = hanning(N)';
-        wavelet = taper .* exp(1i * 2 * pi * f .* (1:N) / fsample);
-        for i_channel = 1:length(data.label)
-            s = data.trial{i_trial}(i_channel, :);
-            c = conv(s, wavelet, 'same');
-            sP = abs(c) .^ 2;
-            p(i_channel, :, i_freq) = sP;
-        end
-    end
-    pwr{i_trial} = p;
+% Set up the wavelets
+wavelets = cell([1 length(freq)]);
+for i_freq = 1:length(freq)
+    f = freq(i_freq);
+    N = floor(width(i_freq) * fsample / f);
+    taper = hanning(N)';
+    wavelet = taper .* exp(1i * 2 * pi * f .* (1:N) / fsample);
+    wavelets{i_freq} = wavelet;
 end
 
-% Append all the data into a matrix
-%   Raw data: Chan x Time
-%   Power: Chan x Time x Freq
-% Pad each trial with NaNs to an integer mult of nfft
-% This ensures that FFTs only cover data from a contiguous segment of data
-append_nan = @(a,n) cat(2, a, nan(size(a, 1), n, size(a, 3)));
-x_padded = cell([length(data.trial) 1]);
-y_padded = cell([length(data.trial) 1]);
-for i_trial = 1:length(data.trial)
-    trial_length = size(data.trial{i_trial}, 2);
-    pad_length = (ceil(trial_length / nfft) * nfft) - trial_length;
-    x_padded{i_trial} = append_nan(data.trial{i_trial}, pad_length);
-    y_padded{i_trial} = append_nan(pwr{i_trial}, pad_length);
-end
-x = cat(2, x_padded{:}); % Raw data
-y = cat(2, y_padded{:}); % Power time-course
-clear x_padded y_padded
-
-% Compute CFC on each channel & frequency
+% Compute CFC
 cfc_data = nan(length(freq), ... % CarFreq x ModFreq x Chan
     nfft, ...
     length(data.label));
+% 
 for i_channel = 1:length(data.label)
     for i_freq = 1:length(freq)
-        
-        % Select data from this chan and freq
-        x_sel = x(i_channel, :);
-        y_sel = y(i_channel, :, i_freq);
+        % Each run through this takes ~1.6 sec
+        % 1.6 s * 304 (channels) * 61 (freqs) = 8.25 hours
+
+        % Compute the power time-course
+        % This loop takes ~0.2 s
+        pwr = cell([1 length(data.trial)]);
+        for i_trial = 1:length(data.trial)
+            s = data.trial{i_trial}(i_channel, :);
+            c = conv(s, wavelet, 'same');
+            sP = abs(c) .^ 2; % Fluctuations in power over time
+            pwr{i_trial} = sP;
+        end
+                
+        % Append all the trials into one big trial separated by NaNs
+        append_nan = @(a, n) cat(2, a, nan(size(a, 1), n));
+        x_padded = cell([length(data.trial) 1]); % Raw data
+        y_padded = cell([length(data.trial) 1]); % Power time-course
+        % This loop takes ~1.3 s
+        for i_trial = 1:length(data.trial)
+            % Pad out to the next multiple of NFFT
+            trial_length = size(data.trial{i_trial}, 2);
+            pad_length = (ceil(trial_length / nfft) * nfft) - trial_length;
+            s = data.trial{i_trial}(i_channel, :); % Raw data
+            x_padded{i_trial} = append_nan(s, pad_length);
+            y_padded{i_trial} = append_nan(pwr{i_trial}, pad_length);
+        end
+        x = cat(2, x_padded{:}); % Raw data
+        y = cat(2, y_padded{:}); % Power time-course
+        clear x_padded y_padded
+
+        % Compute the CFC
         
         % Split each signal into overlapping segments
         seg_length = nfft;
         seg_overlap = 0.5;
         seg_overlap = round(nfft * seg_overlap);
         buf = @(z) buffer(z, seg_length, seg_overlap, 'nodelay');
-        x_s = buf(x_sel);
-        y_s = buf(y_sel);
+        x_s = buf(x);
+        y_s = buf(y);
 
         % Apply a hanning taper to each segment
         taper = @(z) bsxfun(@times, z, hann(nfft));
@@ -81,7 +82,6 @@ for i_channel = 1:length(data.label)
         % Compute FFT of each segment
         X_s = fft(x_s, [], 1);
         Y_s = fft(y_s, [], 1);
-        mod_freq = (0:(nfft - 1)) * fsample / nfft;
 
         % Compute the cross-spectra
         xspec = X_s .* conj(Y_s);
@@ -92,9 +92,13 @@ for i_channel = 1:length(data.label)
         denom = sqrt(nansum(abs(X_s).^2, 2) .* nansum(abs(Y_s).^2, 2));
         cfc = num ./ denom;
         cfc_data(i_freq, :, i_channel) = cfc;
+
     end
 end
 
 % Only keep the real mod freqs
-cfc_data = cfc_data(:, 1:floor(end/2), :, :); 
-mod_freq = mod_freq(1:floor(end/2));
+n_keep_freqs = floor(size(cfc_data, 2) / 2);
+cfc_data = cfc_data(:, 1:n_keep_freqs, :); 
+
+mod_freq = (0:(nfft - 1)) * fsample / nfft;
+mod_freq = mod_freq(1:n_keep_freqs);
