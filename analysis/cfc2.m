ebdca9ac5ee53514de33d7cd1f0770dad536b053
gbrookshire <g.brookshire@bham.ac.uk>
2019-05-08 09:27:22 +0000

    Revert to old CFC - new one was weird

--- analysis/cfc2.m
+++ analysis/cfc2.m
@@ -32,9 +32,8 @@ clear wavelet
 cfc_data = nan(length(freq), ... % CarFreq x ModFreq x Chan
     nfft, ...
     length(data.label));
-
+% 
 for i_channel = 1:length(data.label)
-    disp(i_channel)
     for i_freq = 1:length(freq)
         % Each run through this takes ~1.6 sec
         % 1.6 s * 304 (channels) * 61 (freqs) = 8.25 hours
@@ -50,18 +49,17 @@ for i_channel = 1:length(data.label)
         end
                 
         % Append all the trials into one big trial separated by NaNs
+        append_nan = @(a, n) cat(2, a, nan(size(a, 1), n));
         x_padded = cell([length(data.trial) 1]); % Raw data
         y_padded = cell([length(data.trial) 1]); % Power time-course
         % This loop takes ~1.3 s
         for i_trial = 1:length(data.trial)
-            s = data.trial{i_trial}(i_channel, :); % Raw data
-            % Detrend the segment
-            s = detrend(s);
             % Pad out to the next multiple of NFFT
             trial_length = size(data.trial{i_trial}, 2);
             pad_length = (ceil(trial_length / nfft) * nfft) - trial_length;
-            x_padded{i_trial} = nanpad(s, pad_length);
-            y_padded{i_trial} = nanpad(pwr{i_trial}, pad_length);
+            s = data.trial{i_trial}(i_channel, :); % Raw data
+            x_padded{i_trial} = append_nan(s, pad_length);
+            y_padded{i_trial} = append_nan(pwr{i_trial}, pad_length);
         end
         x = cat(2, x_padded{:}); % Raw data
         y = cat(2, y_padded{:}); % Power time-course
@@ -105,12 +103,3 @@ cfc_data = cfc_data(:, 1:n_keep_freqs, :);
 
 mod_freq = (0:(nfft - 1)) * fsample / nfft;
 mod_freq = mod_freq(1:n_keep_freqs);
-end
-
-function b = nanpad(a, n)
-% Pad an array <a> with <n> nans, distributed across the front and back
-nrows = size(a, 1);
-pre = nan(nrows, floor(n / 2));
-post = nan(nrows, ceil(n / 2));
-b = cat(2, pre, a, post);
-end
