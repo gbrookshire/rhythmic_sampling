b8d6157975ab50ac2b9e7f7b7c96046709c5084d
gbrookshire <g.brookshire@bham.ac.uk>
2019-05-10 08:54:52 +0000

    Separate TFR for determining which channels show an alpha depression after stimulus onset, compared with baseline

---
+++ analysis/rs_baseline_alpha.m
@@ -0,0 +1,34 @@
+function rs_baseline_alpha(i_subject)
+
+% Identify channels that show a difference in alpha response after stimuli
+% appear on the screen.
+
+rs_setup
+
+fname = subject_info.meg{i_subject};
+
+step_size = 0.05; % Should be divisible by 1/Fs to preserve time-bins
+toi = -1:step_size:2;
+
+% Set up dir for saving data
+save_dir = [exp_dir 'tfr/trial/'];
+[~,~,~] = mkdir(save_dir, fname);
+
+% Load preprocessed data
+d = load([exp_dir 'preproc/trial/' fname '/preproc']);
+d = d.data;
+
+% Compute TFRs
+cfg = [];
+cfg.method = 'mtmconvol';
+cfg.taper = 'hanning';
+cfg.toi = toi;
+cfg.keeptrials = 'no';
+cfg.output = 'pow';
+cfg.foi = 3:30;
+cfg.t_ftimwin = 0.5 * ones(size(cfg.foi));
+cfg.pad = 7;
+cfg.padtype = 'mirror';
+tfr = ft_freqanalysis(cfg, d);
+
+save([save_dir '/' fname '/baseline_alpha'], 'tfr')
