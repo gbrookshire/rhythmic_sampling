7fbbc89d332efa309bd6347988396946bbc11539
gbrookshire <g.brookshire@bham.ac.uk>
2019-05-13 16:25:24 +0000

    Improve alpha peaks simulation

--- analysis/rs_simulate_alpha_peaks.m
+++ analysis/rs_simulate_alpha_peaks.m
@@ -10,6 +10,8 @@ fsample = 1000;
 t = -0.5:(1/fsample):4.5;
 n_trials = 336;
 trials = cell(1, n_trials);
+alpha_amp = 1;
+rft_amp = 0.75;
 
 for i_trial = 1:n_trials
     % Make alpha
@@ -19,38 +21,31 @@ for i_trial = 1:n_trials
     [b,a] = butter(5, [f_low, f_high] / (fsample / 2), 'bandpass');
     x_filt = filter(b, a, x_noise);
     x_filt = x_filt - min(x_filt);
-    x_filt = x_filt / max(x_filt);
-
-%     plot(t, x_noise, '-k')
-%     hold on
-%     plot(t, x_filt, '-r')
-%     hold off
+    x_filt = alpha_amp * x_filt / max(x_filt);
 
     % Make RFT signals
     f1 = 63;
     f2 = 78;
-    rft = @(f) (1/2) * (1 + sin(t * f * 2 * pi));
+    rft = @(f) rft_amp * (1/2) * (1 + sin(t * f * 2 * pi));
     rft1 = rft(f1);
     rft2 = rft(f2);
 
     % Combine them
-    % Square alpha power to limit the duty-cycle of RFT oscillations
-    x_comb1 = rft1 .* (x_filt .^ 2) + x_filt;
-    x_comb2 = rft2 .* ((1 - x_filt) .^ 2) + x_filt;
-
-%     plot(t, x_comb1, t, x_comb2);
-%     xlim([0 0.5])
-
-    % Add noise
-    noise_amp = 0.25;
-    y1 = x_comb1 + (noise_amp * rand(size(t)));
-    y2 = x_comb2 + (noise_amp * rand(size(t)));
-
-%     plot(t, y1, t, y2)
-%     xlim([0 0.5])
+    s = 4; % 'Sharpness' exponent - higher numbers reduce the duty cycle
+    c1 = rft1 .* (x_filt .^ s);
+    c2 = rft2 .* ((1 - x_filt) .^ s);
+    c1 = detrend(c1);
+    c2 = detrend(c2);
+    c_comb = c1 + c2;
 
     % Make into three channels with a different mix of each signal
-    y = [y1; y1 + y2; y2];
+    y = [c1; c_comb; c2;];
+    y = y + x_filt;
+    
+    % Add noise
+    noise_amp = 0.0;
+    y = y + (noise_amp * rand(size(y)));   
+    
     trials{i_trial} = y;
 end
     
