bb53f57e4c305fada27f4a6585d22b0b6ba348f9
gbrookshire <g.brookshire@bham.ac.uk>
2019-06-17 16:08:52 +0000

    Topo-plots for the boiled-down effect

--- analysis/rs_acc_hf_hitsides.m
+++ analysis/rs_acc_hf_hitsides.m
@@ -5,13 +5,16 @@ rs_setup
 sides = {'left' 'right'};
 
 segment_type = 'target'; % target | trial
-w0in_size = 0.1;
+% win_size = 0.1;
 
 % win_str = sprintf('win_%.1fs', win_size);
-vers = 'raw_chans';
+vers = 'raw_chans/all';
 tfr_dir = [exp_dir 'tfr/' vers '/' segment_type '/'];
 
-clear x
+occip_roi = {'MEG2042' 'MEG2043' 'MEG2032' 'MEG2033'};
+
+clear x % Subject x Channel X TaggedFreq x Time
+
 for i_subject = 1:height(subject_info)
     if subject_info.exclude(i_subject)
         x(i_subject,:,:,:) = nan;
@@ -21,6 +24,7 @@ for i_subject = 1:height(subject_info)
     % Load target-segmented HF TFR
     % Read in the data segmented around targets
     fname = subject_info.meg{i_subject};
+    disp(fname)
     fn = [tfr_dir fname '/high'];
     d = load(fn);
     d = d.high_freq_data;
@@ -68,20 +72,30 @@ end
 
 for i_sensor_side = 1:2
     
-    %{
-    % RESS side is coded as _stimulus_ side. Flip around to get to the
-    % _sensor_ side.
-    i_ress_side = mod(i_sensor_side, 2) + 1;
-    chan_inx = i_ress_side;
-    %}
-    
-    % Select channels for the raw-channel (non-RESS) analysis
-    if i_sensor_side == 1
-        chan_inx = 3:4; % Sensors on the left side
-    elseif i_sensor_side == 2
-        chan_inx = 1:2;
+    switch vers
+        case 'ress/win_0.1.s'
+            % RESS side is coded as _stimulus_ side. Flip around to get to the
+            % _sensor_ side.
+            i_ress_side = mod(i_sensor_side, 2) + 1;
+            chan_inx = i_ress_side;
+        case 'raw_chans/occip'
+            % Select channels for the raw-channel (non-RESS) analysis
+            if i_sensor_side == 1
+                chan_inx = 3:4; % Sensors on the left side
+            elseif i_sensor_side == 2
+                chan_inx = 1:2;
+            end
+        case 'raw_chans/all'
+            if i_sensor_side == 1
+                chan_inx = 3:4; 
+            elseif i_sensor_side == 2
+                chan_inx = 1:2;
+            end
+            chan_inx = ismember(d.label, occip_roi(chan_inx));
+        otherwise
+            error('Not implemented for TFR version %s', vers)
     end
-    
+        
     for i_tagged_freq = 1:2
         subplot(2, 2, (i_tagged_freq - 1) * 2 + i_sensor_side)
         mi = squeeze(mean(x(:,chan_inx,i_tagged_freq,:), 2));
@@ -106,13 +120,21 @@ print('-dpng',...
 
 %% Plot the difference between the right and left sides
 
-% % For selecting RESS channels
-% left_inx = 2;
-% right_inx = 1;
-
-% For selecting raw channels
-left_inx = 3:4;
-right_inx = 1:2;
+switch vers
+    case 'ress/win_0.1.s'
+        % For selecting RESS channels
+        left_inx = 2;
+        right_inx = 1;
+    case 'raw_chans/occip'
+        % For selecting raw channels
+        left_inx = 3:4;
+        right_inx = 1:2;
+    case 'raw_chans/all'
+        left_inx = ismember(d.label, occip_roi(3:4));
+        right_inx = ismember(d.label, occip_roi(1:2));
+    otherwise
+        error('Not implemented for TFR version %s', vers)
+end
 
 for i_tagged_freq = 1:2
     subplot(2, 1, i_tagged_freq)
@@ -137,4 +159,89 @@ end
 print('-dpng',...
     [exp_dir 'plots/accuracy/high_freq/left-hits_vs_right-hits_diff'])
 
+
+%% Collapse over the two RFT frequencies
+
+switch vers
+    case 'ress/win_0.1.s'
+        % For selecting RESS channels
+        left_inx = 2;
+        right_inx = 1;
+    case 'raw_chans/occip'
+        % For selecting raw channels
+        left_inx = 3:4;
+        right_inx = 1:2;
+    case 'raw_chans/all'
+        left_inx = ismember(d.label, occip_roi(3:4));
+        right_inx = ismember(d.label, occip_roi(1:2));
+    otherwise
+        error('Not implemented for TFR version %s', vers)
+end
+
+mi_left = squeeze(mean(mean(x(:, left_inx, :, :), 2), 3));
+mi_right = squeeze(mean(mean(x(:, right_inx, :, :), 2), 3));
+mi_diff = mi_right - mi_left;
+mi_diff_mean = nanmean(mi_diff, 1); 
+plot(d_l.time, mi_diff, 'color', 0.7 * [1 1 1])
+hold on
+plot([-0.5 0.5], [0 0], '-k')
+plot([0 0], [min(min(mi_diff)) max(max(mi_diff))], '-k')
+plot(d_l.time, mi_diff_mean, '-r', 'LineWidth', 2)
+title(sprintf('%i Hz', ...
+    exp_params.tagged_freqs(i_tagged_freq)));
+ylim([min(min(mi_diff)) max(max(mi_diff))])
+% Simple stats
+[h, p] = ttest(mi_diff, 0, 'dim', 1, 'alpha', 0.05);
+plot(d_l.time(logical(h)), zeros([1 sum(h)]), '*b')
+hold off 
+
+print('-dpng',...
+    [exp_dir 'plots/accuracy/high_freq/left-hits_vs_right-hits_diff-overall'])
+
+
+%% Plot the topography of this effect
+
+% x: Subject x Channel X TaggedFreq x Time
+x_m = squeeze(nanmean(mean(x, 3), 1));
+
+% Set up FT object
+d_mi = [];
+d_mi.label = d.label;
+d_mi.time = d.time;
+d_mi.avg = x_m;
+
+% Combine planar gradiometers
+fn = [exp_dir 'grad/' fname '/grad'];
+grad = load(fn);
+d_mi.grad = grad.grad;
+cfg = [];
+cfg.method = 'sum';
+cfg.updatesens = 'yes';
+d_mi = ft_combineplanar(cfg, d_mi);
+
+% Plot it
+t_centers = (-1 * [0.4 0.3 0.2 0.1 0]');
+for i_plot = 1:length(t_centers)
+    subplot(1, length(t_centers), i_plot)
+    cfg = [];
+    cfg.layout = chan.grad_cmb.layout;
+    cfg.xlim = t_centers(i_plot) + [-0.01 0.01];
+    cfg.zlim = 'maxabs';
+    cfg.colorbar = 'no';
+    cfg.style = 'straight';
+    cfg.comment = 'no';
+    cfg.shading = 'interp';
+    cfg.markersymbol = '.';
+    cfg.gridscale = 200;
+    cfg.colorbar = 'yes';
+    cfg.highlight = 'on';
+    cfg.highlightchannel = {'MEG2042+2043' 'MEG2032+2033'};
+    cfg.highlightsymbol = '.';
+    cfg.highlightsize = 10;
+    ft_topoplotER(cfg, d_mi)
+    title(sprintf('t = [%.2f, %.2f]', cfg.xlim(1), cfg.xlim(2)));
+end
+
+fn = [exp_dir 'plots/accuracy/high_freq/left-hits_vs_right-hits_topo'];
+print('-dpng', fn)
 
