2161f753e2ebbea8c02675c3e5e1f12707fac02a
gbrookshire <g.brookshire@bham.ac.uk>
2019-08-15 12:40:20 +0000

    Coh using synth freq for non-measured left stimulus

--- analysis/rs_coherence.m
+++ analysis/rs_coherence.m
@@ -1,84 +1,219 @@
-function rs_coherence(i_subject)
-
 % Compute coherence between the MEG recordings and the photodiode
 % 
-% Get coherence separately for hits-on-the-left, and hits-on-the-right
-% Separate by frequency contralateral to the stimulus
+% Overall -- just check how coherence is working
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% We only have photodiode recordings for the stim on the right %
+% To get around this, we can synthesize stim signals on the left
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+clear variables
+close all
 rs_setup
 
-fname = subject_info.meg{i_subject};
+% How should the hits and misses be compared
+% cmp_fnc = @(a,b) a - b;
+cmp_fnc = @(a,b) (a - b) ./ (a + b);
 
-% Load preprocessed data
-behav = rs_behavior(i_subject);
-d_meg = rs_preproc_ress(i_subject, 'target');
-d_photo = load([exp_dir 'preproc/photodiode/target/' fname '/preproc']);
-d_photo = d_photo.data;
-data = ft_appenddata([], d_meg, d_photo);
-data.trialinfo = d_meg.trialinfo;
-clear d_meg d_photo1
+% The stimuli were not shown at exactly the intended frequencies, due to
+% the fact that the screen was not refreshed at exactly 120 Hz. These
+% adjustments here fix the difference in frequencies for the synth signals.
+freq_adjust = [-0.0535 -0.0648]; % For 63 and 78 Hz, respectively
 
-% Compute TFR 
-cfg = [];
-cfg.output = 'fourier'; % FUll Fourier representation needed for coherence
-cfg.method = 'mtmconvol';
-cfg.taper = 'hanning';
-cfg.foi = 55:100;
-cfg.toi = -0.5:0.05:0.5;
-cfg.t_ftimwin = ones(length(cfg.foi),1) .* 0.1;
-cfg.keeptrials = 'yes';
-fourier = ft_freqanalysis(cfg,data);
+clear cohspctrm_overall % Dim: subject * [cohspctrm]
 
-% Get rid of trials with nans
-% And keep only hits
-nan_trials = any(squeeze(isnan(fourier.fourierspctrm(:,1,1,:))), 2);
-hits = fourier.trialinfo(:,1) == 1;
-cfg = [];
-cfg.trials = hits & ~nan_trials;
-% cfg.trials = ~nan_trials;
-fourier = ft_selectdata(cfg, fourier);
+for i_subject = 1:height(subject_info)
+    if subject_info.exclude(i_subject)
+        continue
+    end
+    fname = subject_info.meg{i_subject};
 
-% Find which side each RFT frequency was on in each trial
-trial_num = fourier.trialinfo(:,2);
-freq_left = behav.freq_left(trial_num);
-target_side = behav.target_side(trial_num);
+    % Load preprocessed data
+    behav = rs_behavior(i_subject);
+    d_meg = rs_preproc_ress(i_subject, 'target');
+    fn_photo = [exp_dir 'preproc/photodiode/target/' fname '/preproc'];
+    d_photo = load(fn_photo);
+    d_photo = d_photo.data;
+    d_photo.label = {'stim_right'};
 
-% Get coherence separately for each mapping of freq to each side
-sides = {'left' 'right'};
-clear cohspctrm % chancmb * freq * time * RFT_mapping * target_side
-clear trial_counts
-for i_rft_map = 1:2
-    % i_freq == 1: left side at 63 Hz, right side at 78 Hz
-    % i_freq == 2: left side at 78 Hz, right side at 63 Hz
-    freq_sel = freq_left == exp_params.tagged_freqs(i_rft_map);
-    for i_targ_side = 1:2
-        % i_targ_side == 1: target is on the left (left-hits)
-        % i_targ_side == 2: target is on the right (right-hits)
-        targside_sel = strcmp(target_side, sides{i_targ_side});
-        % Combine selection vectors to get trial selection
-        trial_sel = freq_sel & targside_sel;
-        trial_counts(i_rft_map, i_targ_side) = sum(trial_sel);
-        % Compute coherence
-        cfg = [];
-        cfg.trials = trial_sel;
-        cfg.method = 'coh';
-        cfg.channelcmb = {'left' 'MISC004'
-                          'right' 'MISC004'};
-        coh = ft_connectivityanalysis(cfg, fourier);
-        cohspctrm(:,:,:,i_rft_map,i_targ_side) = coh.cohspctrm;
+    % Synthesize surrogate photodiode response for the
+    % non-measured left stim
+    d_synth = d_photo;
+    d_synth.label = {'stim_left'};
+    trial_num = d_meg.trialinfo(:,2);
+    for i_trial = 1:length(d_photo.trial)
+        % Adjust freq using measured diff in freq due to the frame
+        % rate being slightly slower than 120 Hz
+        f = behav.freq_left(trial_num(i_trial)); % Freq on left in this trial
+        f = f + freq_adjust(exp_params.tagged_freqs == f);
+        t = d_photo.time{i_trial};
+        y = sin(2 * pi * f * t);
+        d_synth.trial{i_trial} = y;
+    end
+    clear f t y
+    
+    % Combine the data structures
+    data = ft_appenddata([], d_meg, d_photo, d_synth);
+    data.trialinfo = d_meg.trialinfo;
+
+    % Compute TFR 
+    cfg = [];
+    cfg.output = 'fourier'; % Full Fourier representation
+    cfg.method = 'mtmconvol';
+    cfg.taper = 'hanning';
+    cfg.foi = 55:100;
+    cfg.toi = -0.5:0.05:0.5;
+    cfg.t_ftimwin = ones(length(cfg.foi),1) .* 0.1;
+    cfg.keeptrials = 'yes';
+    fourier = ft_freqanalysis(cfg,data);
+
+    % Get rid of trials with nans
+    % And keep only hits
+    nan_trials = any(squeeze(isnan(fourier.fourierspctrm(:,1,1,:))), 2);
+    % hits = fourier.trialinfo(:,1) == 1;
+    cfg = [];
+    cfg.trials = ~nan_trials;
+    fourier = ft_selectdata(cfg, fourier);
+
+    % Find which side each RFT frequency was on in each trial
+    trial_num = fourier.trialinfo(:,2);
+    freq_left = behav.freq_left(trial_num);
+
+    % Get coherence separately for each mapping of freq to each side
+    clear cohspctrm % chancmb * freq * time * RFT_mapping * hit
+    clear trial_counts
+    for i_rft_map = 1:2
+        for hit = 0:1
+            % i_rft_map == 1: left side at 63 Hz, right side at 78 Hz
+            % i_rft_map == 2: left side at 78 Hz, right side at 63 Hz
+            hit_sel = fourier.trialinfo(:,1) == hit;
+            freq_sel = freq_left == exp_params.tagged_freqs(i_rft_map);
+            trial_sel = hit_sel & freq_sel;
+
+            trial_counts(i_rft_map,hit+1) = sum(trial_sel);
+            % Compute coherence
+            cfg = [];
+            cfg.trials = trial_sel;
+            cfg.method = 'coh';
+            cfg.channelcmb = {'left' 'stim_left';
+                              'right' 'stim_right'};
+            coh = ft_connectivityanalysis(cfg, fourier);
+            cohspctrm(:,:,:,i_rft_map,hit+1) = coh.cohspctrm;
+            cohspctrm_overall(i_subject,:,:,:,i_rft_map,hit+1) = coh.cohspctrm;
+        end
+    end
+
+    % Clean out the raw coherence spectrum so we don't get confused
+    coh = rmfield(coh, 'cohspctrm');
+
+    % Remember that the RESS filters are named after the stimulus that 
+    % they're designed to select. So 'left' refers to a filter that 
+    % selects stimuli on the left side -- not the channels on the 
+    % left side of the head.
+
+    % Plot it
+    for i_chancmb = 1:2
+        figure(i_chancmb)
+        for i_rft_map = 1:2
+            for hit = 0:2
+                subplot(3, 2, (hit * 2) + i_rft_map)
+
+                if hit == 2 % Compare Hits vs Misses
+                    x_m = cohspctrm(i_chancmb, :, :, i_rft_map,1);
+                    x_h = cohspctrm(i_chancmb, :, :, i_rft_map,2);
+                    x = cmp_fnc(x_h, x_m); % Set at the top of the script
+                else % For hits or misses alone
+                    x = cohspctrm(i_chancmb, :, :, i_rft_map,hit+1);
+                end
+                x = squeeze(x);
+
+                imagesc(coh.time, ...
+                    coh.freq, ...
+                    x)
+                set(gca, 'YDir', 'normal')
+                colorbar;
+            end
+        end
+
+        subplot(3,2,1)
+        hold on
+        t = text(-1, 80, 'Miss');
+        t.FontSize = 12;
+        title('Right stim: 78 Hz')
+        hold off
+
+        subplot(3,2,2)
+        title('Right stim: 63 Hz')
+
+        subplot(3,2,3)
+        hold on
+        t = text(-1, 80, 'Hit');
+        t.FontSize = 12;
+        hold off
+
+        subplot(3,2,5)
+        hold on
+        t = text(-1, 80, 'Diff');
+        t.FontSize = 12;
+        hold off
+
+        % Save a plot of this
+        fn = strrep(fname, '/', '_');
+        fn = [fn '-' coh.labelcmb{i_chancmb} '_stim'];
+        save_fname = [exp_dir 'plots/coherence/hit_miss/' fn];
+        print('-dpng', save_fname)
     end
 end
 
-% Get left-hits vs right-hits
-x_l = cohspctrm(:,:,:,:,1);
-x_r = cohspctrm(:,:,:,:,2);
-x = (x_l - x_r) ./ (x_l + x_r);
-% Dimensions of x: chancmb * freq * time * RFT_freq_mapping
+%% Plot the average over subjects
 
-% Clean out the raw coherence spectrum so we don't get confused
-coh = rmfield(coh, 'cohspctrm');
+for i_chancmb = 1:2
+    figure(i_chancmb)
+    for i_rft_map = 1:2
+        for hit = 0:2
+            subplot(3, 2, (hit * 2) + i_rft_map)
 
-% Save the data
-save_dir = [exp_dir 'coherence/'];
-[~,~,~] = mkdir(save_dir, fname);
-save([save_dir '/' fname '/coh'], 'coh', 'x', 'cohspctrm')
+            if hit == 2 % Hits vs Misses
+                x_m = cohspctrm_overall(:,i_chancmb, :, :, i_rft_map,1);
+                x_h = cohspctrm_overall(:,i_chancmb, :, :, i_rft_map,2);
+                x = cmp_fnc(x_h, x_m);
+            else
+                x = cohspctrm_overall(:,i_chancmb, :, :, i_rft_map,hit+1);
+            end
+            x = nanmean(x, 1);
+            x = squeeze(x);
+
+            imagesc(coh.time, ...
+                coh.freq, ...
+                x)
+            set(gca, 'YDir', 'normal')
+            colorbar;
+        end
+    end
+
+    subplot(3,2,1)
+    hold on
+    t = text(-1, 80, 'Miss');
+    t.FontSize = 12;
+    title('Right stim: 78 Hz')
+    hold off
+
+    subplot(3,2,2)
+    title('Right stim: 63 Hz')
+
+    subplot(3,2,3)
+    hold on
+    t = text(-1, 80, 'Hit');
+    t.FontSize = 12;
+    hold off
+
+    subplot(3,2,5)
+    hold on
+    t = text(-1, 80, 'Diff');
+    t.FontSize = 12;
+    hold off
+
+    % Save a plot of this
+    fn = ['avg' '-' coh.labelcmb{i_chancmb} '_stim'];
+    save_fname = [exp_dir 'plots/coherence/hit_miss/' fn];
+    print('-dpng', save_fname)
+end
